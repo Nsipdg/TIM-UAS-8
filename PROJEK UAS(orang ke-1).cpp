d5d3deb1203f08db85d110449eb11f49aaf5abab
HERI SETIAWAN <mherisetiawan89@gmail.com>
2025-01-04 17:36:15 +0700

    Update PROJEK UAS(orang ke-1).cpp

--- PROJEK UAS(orang ke-1).cpp
+++ PROJEK UAS(orang ke-1).cpp
@@ -1,66 +1,63 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-int main ()
-{
-	Cout<<"## PROGRAM APLIKASI KASIR ##<<endl;
-	cout<<"===========================<<endl<<endl;
-	const int MAX_BARANG = 10;
-	string namabarang [MAX_BARANG];
-	long hargabarang [MAX_BARANG];
-	int jumlahbarang [MAX_BARANG];
-	long jumlahtotalbarang;
-	long totalharga=0;
-	long jumlahbayar=0;
 
-	cout<<"## SELAMAT DATANG DI MINI MARKET 08 ##"<<endl<<endl;
-	cout<<"Masukan Jumlah Barang Yang Ingin Dibeli : ";cin>>jumlahtotalbarang;
-	cout<<endl;
+int main()
+{  
+    cout<<"##   Program Aplikasi Kasir  ##"<<endl;
+    cout<<"==============================="<<endl<<endl;
 
-	if(jumlahtotalbarang<=0 || jumlahtotalbarang>MAX_BARANG){
-		cout<<"Jumlah Barang Tidak Valid"<<endl;
-		return 0;
-	}
-	for(int i=0;i<jumlahtotalbarang;i++)
-		{
-			cout<<"Barang ke : "<<i+1<<endl;
-			cout<<"Nama Barang : ";
-			getline(cin>>ws,namabarang[i]);
-			cout<<"Harga Satuan :";
-			cin>>hargabrang[i];
-			cout<<"Jumlah :"<<namabarang[i]<<"yang dibeli :";
-			cin>>jumlahbarang[i];
-			cout<<endl;
-		}
-	cout<<endl;
+    const int MAX_BARANG = 10;
+    string namabarang [MAX_BARANG];
+    long hargabarang [MAX_BARANG];
+    int jumlahbarang [MAX_BARANG];
+    long jumlahtotalbarang;
+    long totalharga = 0;
+    long jumlahbayar = 0;
 
-	cout<<"===================================="<<endl;
-	cout<<"##      STRUK MINIMARKET 08      ##"<<endl;
-	cout<<"===================================="<<endl;
-	cout<<"No Barang Jumlah Harga Satuan SubTotal"<<endl;
-	for(int i=0;i<jumlahtotalbarang;i++){
-		cout<<setw(1)<<1+1<<" ";
-		cout<<left<<setw(12)<<namabarang[i];
-		cout<<right<<setw(15)<<jumlahbarang[i];
-		cout<<setw(18)<<hargabarang[i];
-		cout<<setw(15)<<jumlahbarang[i]*hargabarang[i]);
-	}
-	cout<<"======================================"<<endl;
-	cout<<"Total Harga : Rp. "<<totalharga<<endl<<endl;
-	cout<<"Jumlah Bayar : Rp. ";
-	cin>>jumlahbayar;
-	cout<<endl;
+    cout<<"##   Selamat Datang di Mini Market 08    ##"<<endl<<endl;
 
-	while(jumlahbayar-totalharga<0){
-		cout<<"Maaf, Uang Anda Kurang.Mohon Lakukan Ulang Pembayaran<<endl;
-			cout<<"Jumlah Bayar : Rp. ";
-		cin>>jumlahbayar:
-			cout<<endl;
-	}
-	cout<<"Kembalian : Rp."<<jumlahbayar-totalharga<<endl<<endl;
-	cout<<"========================================="<<endl;
-	cout<<"## TERIMA KASIH SUDAH BERBELANJA ##<<endl;
-		cout<<"========================================"<<endl;
+    cout<<"Masukan jumlah barangan yangan ingin dibeli : "; cin>>jumlahtotalbarang; cout<<endl;
+    
+    if (jumlahtotalbarang <= 0 || jumlahtotalbarang > MAX_BARANG){
+        cout<<"Jumlah barang tidak valid"<<endl;
+        return 0;
+    }
+    for (int i=0; i<jumlahtotalbarang; i++){
+        cout<<"Barang ke : "<<i+1<<endl;
+        cout<<"Nama Barang : ";
+        getline(cin>>ws,namabarang[i]);
+        cout<<"Harga Satuan : "; cin>>hargabarang[i];
+        cout<<"Jumlah : "<<namabarang[i]<<" yang dibeli : "; cin>>jumlahbarang[i]; 
+        cout<<endl;
+    }cout<<endl;
 
-	return 0:
+	cout<<"------------------------------------------------------"<<endl;
+    cout<<"####----      Struk Mini Market 08           ----####"<<endl;
+    cout<<"------------------------------------------------------"<<endl;
+    cout<<"NO   Barang  Jumlah  Harga Satuan    Sub Total"<<endl;
+    for(int i = 0; i < jumlahtotalbarang; i++){
+        cout<<setw(1)<<1+1<<" ";
+        cout<<left<<setw(12)<<namabarang[i];
+        cout<<right<<setw(15)<<jumlahbarang[i];
+        cout<<setw(18)<<hargabarang[i];
+        cout<<setw(15)<<jumlahbarang[i]*hargabarang[i];
+        cout<<endl;
+        totalharga=totalharga+(jumlahbarang[i]*hargabarang[i]);
+    }
+    cout<<"------------------------------------------------------"<<endl;
+    cout<<"Total Harga : Rp. "<<totalharga<<endl<<endl;
+    cout<<"Jumlah Bayar : Rp. "; cin>>jumlahbayar; cout<<endl;
+    
+    while(jumlahbayar-totalharga<0){
+        cout<<"Maaf, Uang anda kurang. Mohon lakukan ulang pembayaran"<<endl;
+        cout<<"Jumlah Bayar : Rp. "; cin>>jumlahbayar; cout<<endl;
+    }
+    cout<<"Kembalian : Rp. "<<jumlahbayar-totalharga<<endl<<endl;
+    cout<<"------------------------------------------------------"<<endl;
+    cout<<"#### Terimakasih sudah berbelanja, datang kembali ####"<<endl;
+    cout<<"------------------------------------------------------"<<endl;
+    
+    return 0;
+    
 }
